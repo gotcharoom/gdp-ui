3cdd584109af4fabcccc7170d21db1f13f63e748
tryoo0607 <tryoo0607@gmail.com>
2025-02-14 01:32:13 +0900

    스타일 수정 - 주석 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
+// Styles
+import './App.css'
 import './styles/main.scss'
 
 function App() {
