0076eaf8340da8ed313ceadd3e48320db33579ce
tryoo0607 <tryoo0607@gmail.com>
2025-02-14 20:40:16 +0900

    소스 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import {RouterProvider} from "react-router-dom";
+import CustomRouter from "@routes/CustomRouter.tsx";
+
 // Styles
 import './App.css'
 import './styles/main.scss'
-import {RouterProvider} from "react-router-dom";
-import CustomRouter from "@routes/CustomRouter.tsx";
+
 
 const App = () => {
 
