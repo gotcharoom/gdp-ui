6fca39bd6ee1ff3a1f774c174e4fff5b4e5f7116
tryoo0607 <tryoo0607@gmail.com>
2025-02-16 23:02:31 +0900

    스타일 수정 - 헤더 스타일 수정

--- src/layout/components/Header.tsx
+++ src/layout/components/Header.tsx
@@ -1,4 +1,5 @@
 import '@styles/layout/components/Header.scss';
+import { Button, IconButton } from '@mui/material';
 
 interface HeaderProps {
     toggleMenu: (isOpen: boolean) => void;
@@ -22,14 +23,24 @@ const Header = (props: HeaderProps) => {
                 <div className={'header__nav__logo'}>
                     <img className={'logo'} alt={'header__logo'} src={'logo/GDP_LOGO.png'} />
                 </div>
-                <div className={'header__nav__menu'} onClick={onClickMenu}>
-                    Menu
+                <div className={'header__nav__menu'}>
+                    <IconButton className={'menu'} onClick={onClickMenu}>
+                        <span className='material-icons'>menu</span>
+                    </IconButton>
                 </div>
             </div>
             <div className={'header__search-bar'}></div>
             <div className={'header__social'}>
-                <div className={'header__social__discord'}>Discord</div>
-                <div className={'header__social__account'}>account</div>
+                <div className={'header__social__discord'}>
+                    <IconButton className={'discord'}>
+                        <img className={'logo'} alt={'header__logo'} src={'logo/Discord_Logo.png'} />
+                    </IconButton>
+                </div>
+                <div className={'header__social__account'}>
+                    <IconButton className={'account'}>
+                        <img className={'logo'} alt={'header__logo'} src={'logo/Discord_Logo.png'} />
+                    </IconButton>
+                </div>
             </div>
         </div>
     );
