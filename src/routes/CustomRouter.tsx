5a8f1f6f3871029ef2eb4372dfa27d0b38343ae3
tryoo0607 <tryoo0607@gmail.com>
2025-02-17 00:11:48 +0900

    경고 제고 위한 router 옵션 추가

--- src/routes/CustomRouter.tsx
+++ src/routes/CustomRouter.tsx
@@ -9,7 +9,17 @@ import { RoutesContext } from '@/contexts/RoutesContext.ts';
 // Custom Router 설정
 const routes: RouteObject[] = [...UserRoutes, ...AdminRoutes];
 
-const CustomRouter = createBrowserRouter(routes);
+const option = {
+    future: {
+        v7_fetcherPersist: true,
+        v7_normalizeFormMethod: true,
+        v7_partialHydration: true,
+        v7_relativeSplatPath: true,
+        v7_skipActionErrorRevalidation: true,
+    },
+};
+
+const CustomRouter = createBrowserRouter(routes, option);
 
 export const RoutesProvider = ({ children }: { children: ReactNode }) => {
     return <RoutesContext.Provider value={routes}>{children}</RoutesContext.Provider>;
