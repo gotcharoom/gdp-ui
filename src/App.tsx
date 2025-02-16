70b0efc4da44dc88d7b3fb451bd078a0be26623c
tryoo0607 <tryoo0607@gmail.com>
2025-02-16 12:32:42 +0900

    스타일 수정 - 기본 스타일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import {RouterProvider} from "react-router-dom";
-import CustomRouter from "@routes/CustomRouter.tsx";
+import { RouterProvider } from 'react-router-dom';
+import CustomRouter from '@routes/CustomRouter.tsx';
 
 // Styles
-import './App.css'
-import './styles/main.scss'
-
+import './styles/main.scss';
 
 const App = () => {
+    return <RouterProvider router={CustomRouter} />;
+};
 
-  return (
-    <RouterProvider router={CustomRouter} />
-  )
-}
-
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {Provider} from "react-redux";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-import App from './App.tsx'
-import {store} from "@stores/store.ts";
+import App from './App.tsx';
+import { store } from '@stores/store.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={store}>
-        <App />
-    </Provider>
-  </StrictMode>,
-)
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>,
+);
