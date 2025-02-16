2f2d33da17eb780af33e13c0490ab249634f7fef
tryoo0607 <tryoo0607@gmail.com>
2025-02-16 12:33:39 +0900

    스타일 수정 - CommonLayout에 스타일 구조 적용

--- src/layout/CommonLayout.tsx
+++ src/layout/CommonLayout.tsx
@@ -5,20 +5,28 @@ import Header from '@layout/components/Header.tsx';
 import Snb from '@layout/components/Snb.tsx';
 import Footer from '@layout/components/Footer.tsx';
 
+import '@styles/layout/CommonLayout.scss';
+
 const CommonLayout = () => {
-    /* state */
+    /* Hooks */
     const matches = useMatches();
     const title = matches.reverse().find((match) => match.handle?.title)?.handle?.title || 'Default Title';
 
+    /* Privates */
+
+    /* Event */
+
+    /* Lifecycle */
+
     return (
-        <>
+        <div className={'common-layout'}>
             <Header />
             <Snb />
             <main>
                 <Outlet context={{ title }} />
             </main>
             <Footer />
-        </>
+        </div>
     );
 };
 
