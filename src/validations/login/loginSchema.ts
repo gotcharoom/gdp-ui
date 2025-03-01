8a2f304acc0c331b9c9eff6cf48a6dbf42bff02e
tryoo0607 <tryoo0607@gmail.com>
2025-03-01 17:08:51 +0900

    Validation Custom 문구 구조 적용

--- src/validations/login/loginSchema.ts
+++ src/validations/login/loginSchema.ts
@@ -1,7 +1,8 @@
 import * as yup from 'yup';
 import { ObjectSchema } from 'yup';
+import message from '@/validations/validationMessages.ts';
 
 export const loginSchema: ObjectSchema<yup> = yup.object().shape({
-    id: yup.string().min(5).required(),
+    id: yup.string().min(5, message.min('id', 5)).required(),
     password: yup.string(),
 });
