781b5358c242e110936c647efe7caec47e621f0f
tryoo0607 <tryoo0607@gmail.com>
2025-03-05 04:13:32 +0900

    vite / package 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig(({ mode }) => {
     return {
         plugins: [react(), tsconfigPaths()],
         server: {
+            port: 5173,
+            strictPort: true,
             proxy: {
                 '/api': {
                     target: env.VITE_API_URL, // 환경 변수 적용
