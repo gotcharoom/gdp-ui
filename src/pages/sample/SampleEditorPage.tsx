280ebf405bee480a26a0518370db6bb509e55d68
tryoo0607 <tryoo0607@gmail.com>
2025-03-09 23:34:08 +0900

    TipTap 에디터 Detail Sample 코드 추가

--- src/pages/sample/SampleEditorPage.tsx
+++ src/pages/sample/SampleEditorPage.tsx
@@ -10,6 +10,9 @@ const SampleEditorPage = () => {
     return (
         <CommonPage width={'100%'} height={'100%'} title={'Sample Editor'}>
             <CommonEditor text={text} setText={setText} />
+
+            {/* Detail Page에서 보여줄 때 => className tiptap 추가 필수*/}
+            <div className='tiptap' dangerouslySetInnerHTML={{ __html: text }} />
         </CommonPage>
     );
 };
