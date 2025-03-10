fb26067b48e47fd5d79f27f0366f4bb9d1ee2e8a
tryoo0607 <tryoo0607@gmail.com>
2025-03-10 16:31:30 +0900

    약관 동의 페이지 작업 완료

--- src/pages/common/AgreementPage.tsx
+++ src/pages/common/AgreementPage.tsx
@@ -1,5 +1,5 @@
 import CommonPage from '@/common/components/CommonPage.tsx';
-import { useOutletContext } from 'react-router-dom';
+import { useNavigate, useOutletContext } from 'react-router-dom';
 
 import '@styles/pages/common/AgreementPage.scss';
 import { Box, Button, Checkbox, FormControlLabel } from '@mui/material';
@@ -9,11 +9,13 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { agreementSchema } from '@/validations/login/agreementSchema.ts';
 import AgreementForm from '@/types/pages/login/AgreementForm.type.ts';
 import * as React from 'react';
-import { useCallback } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
+import GdpTerms from '@pages/common/components/GdpTerms.tsx';
 
 const AgreementPage = () => {
     /* Hooks */
     const { title } = useOutletContext<{ title: string }>();
+    const navigate = useNavigate();
     const method = useForm<AgreementForm>({
         resolver: yupResolver(agreementSchema),
         defaultValues: {
@@ -27,6 +29,10 @@ const AgreementPage = () => {
     /* Privates */
     const hasAllChecked = hasAcceptedGdpTerms && hasAcceptedPrivateTerms;
 
+    const routeToSingUp = useCallback(() => {
+        navigate('/login/sign-up', { state: { agreed: true } });
+    }, [navigate]);
+
     /* Events */
     const onClickAllCheck = useCallback(() => {
         let checked = true;
@@ -37,7 +43,9 @@ const AgreementPage = () => {
         method.setValue('hasAcceptedPrivateTerms' as keyof AgreementForm, checked);
     }, [hasAllChecked, method]);
 
-    const onSubmit = useCallback(() => {}, []);
+    const onSubmit = useCallback(() => {
+        routeToSingUp();
+    }, [routeToSingUp]);
 
     /* Lifecycles */
 
@@ -47,16 +55,20 @@ const AgreementPage = () => {
                 <form onSubmit={method.handleSubmit(onSubmit)}>
                     <div className={'agreement-page__terms-container'}>
                         <ControlCheckbox control={method.control} field='hasAcceptedGdpTerms' label={'GDP 이용약관'} />
-                        <Box className={'agreement-page__container__box'}>testetstst</Box>
+                        <Box className={'agreement-page__container__box'}>
+                            <GdpTerms />
+                        </Box>
                     </div>
                     <div className={'agreement-page__terms-container'}>
                         <ControlCheckbox control={method.control} field='hasAcceptedPrivateTerms' label={'개인정보 이용 및 수집 동의'} />
-                        <Box className={'agreement-page__container__box'}>testetstst</Box>
+                        <Box className={'agreement-page__container__box'}>
+                            <GdpTerms />
+                        </Box>
                     </div>
                     <div className={'agreement-page__option-container'}>
                         <FormControlLabel control={<Checkbox checked={hasAllChecked} onChange={onClickAllCheck} />} label={'전체 동의'} />
                         <Button className={'agreement-page__submit'} variant='contained' type='submit'>
-                            로그인
+                            다음
                         </Button>
                     </div>
                 </form>
