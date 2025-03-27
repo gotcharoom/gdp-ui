e19141612c04d78eac80e21a61c34a73fe6f8129
tryoo0607 <tryoo0607@gmail.com>
2025-03-27 18:20:04 +0900

    알림 css 작업 중

--- src/common/layout/components/NotificationBadge.tsx
+++ src/common/layout/components/NotificationBadge.tsx
@@ -5,21 +5,37 @@ import { Notification, NotificationType } from '@/common/contexts/SseContext.ts'
 import '@styles/layout/components/NotificationBadge.scss';
 import { putReadNotification } from '@apis/notification/notification.ts';
 import { useSse } from '@/common/hooks/useSse.ts';
+import { useNavigate } from 'react-router-dom';
+import clsx from 'clsx';
 
 const NotificationBadge = () => {
     /* Hooks */
     const [open, setOpen] = useState(false);
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const { events, removeEvent } = useSse();
+    const navigate = useNavigate();
+
     /* Privates */
-    const badgeCount = useMemo(() => {
-        return events.filter((event) => event.notificationType !== NotificationType.SYSTEM).filter((event) => !event.isRead).length;
+    const filteredEvents = useMemo(() => {
+        return events.filter((event) => event.notificationType !== NotificationType.SYSTEM).filter((event) => !event.isRead);
     }, [events]);
+
+    const badgeCount = useMemo(() => {
+        return filteredEvents.length;
+    }, [filteredEvents]);
 
     /* Events */
     const onClickBadge = useCallback((event: MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget);
         setOpen((previousOpen) => !previousOpen);
+    }, []);
+
+    const onClickNavigate = useCallback((url: string | undefined) => {
+        if (url == undefined) {
+            return;
+        }
+
+        navigate(url);
     }, []);
 
     const onClickRead = useCallback(
@@ -45,15 +61,20 @@ const NotificationBadge = () => {
                         <Typography className={'badge__popper__paper__empty-notification'}>알림이 없습니다.</Typography>
                     ) : (
                         <List className={'badge__popper__paper__list'}>
-                            {events.map((notification, idx) => (
+                            {filteredEvents.map((notification, idx) => (
                                 <div key={idx}>
                                     <ListItem className={'badge__popper__paper__list__item'}>
-                                        <ListItemText primary={notification.content} secondary={notification.toName} />
+                                        <ListItemText
+                                            className={clsx(notification.url ? 'badge__popper__paper__list__item--hover' : '')}
+                                            primary={notification.content}
+                                            secondary={notification.toName}
+                                            onClick={notification.url ? () => onClickNavigate(notification.url) : undefined}
+                                        />
                                         <Button variant='text' onClick={() => onClickRead(notification)}>
                                             읽음
                                         </Button>
                                     </ListItem>
-                                    {idx < events.length - 1 && <Divider />}
+                                    {idx < filteredEvents.length - 1 && <Divider />}
                                 </div>
                             ))}
                         </List>
